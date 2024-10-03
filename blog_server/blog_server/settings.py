171956e7e7ce48670285ad285818c2b2dff6a6d6
AntonYakovlev <120049919+L1enSet@users.noreply.github.com>
2024-10-04 00:20:42 +0500

    04/10/224

--- blog_server/blog_server/settings.py
+++ blog_server/blog_server/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog_db',
-        'USER': 'postgres',
+        'USER': 'admin',
         'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': 5432,
